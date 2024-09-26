05c934f9817c4f5f7e9b6c218c1b03e9a0d22176
IgnPine <ipineda@alumno.huergo.edu.ar>
2024-09-26 20:45:10 -0300

    reservas finalizadas pe

--- test_sistemareserva.py
+++ test_sistemareserva.py
@@ -29,7 +29,36 @@ class TestDate(unittest.TestCase):
         sistema.add_room(sala1)
         sistema.book_room("Sala 1", f1)
 
-        sistema.get_bookings_for_day(DateTime(2024, 11, 23, 23, 00))
+        res = sistema.get_bookings_for_day(DateTime(2024, 11, 23, 23, 00))
+        self.assertEqual([("Sala 1", ["23/11/2024 - 19:00 | 24/11/2024 - 00:00"])], res)
+
+    def test_reserva_se_cancela(self):
+        f1 = Interval(DateTime(2024, 11, 23, 19, 00), DateTime(2024, 11, 24, 00, 00))
+
+        sala1 = Room("Sala 1", 15)
+        sistema = BookingSystem()
+
+        sistema.add_room(sala1)
+        sistema.book_room("Sala 1", f1)
+        sistema.remove_booking("Sala 1", f1)
+
+        res = sistema.get_bookings_for_day(DateTime(2024, 11, 23, 23, 00))
+        self.assertEqual([], res)
+
+    def test_reserva_se_cancela_con_misma_fecha_distinto_objeto(self):
+        f1 = Interval(DateTime(2024, 11, 23, 19, 00), DateTime(2024, 11, 24, 00, 00))
+        f2 = Interval(DateTime(2024, 11, 23, 19, 00), DateTime(2024, 11, 24, 00, 00))
+
+        sala1 = Room("Sala 1", 15)
+        sistema = BookingSystem()
+
+        sistema.add_room(sala1)
+        sistema.book_room("Sala 1", f1)
+        sistema.remove_booking("Sala 1", f2)
+
+        res = sistema.get_bookings_for_day(DateTime(2024, 11, 23, 23, 00))
+        self.assertEqual([], res)
+
 
 if __name__ == "__main__":
     unittest.main() 
